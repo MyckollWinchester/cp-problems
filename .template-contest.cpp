4f4aa361fd59373747302dbb12cc71030aae44df
MyckollWinchester <myckoll.winchester@usm.cl>
2024-10-13 14:25:36 -0300

    added contest template

---
+++ .template-contest.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+typedef long long ll;
+template<typename T> constexpr T inf=0;
+template<> constexpr int inf<int> = 0x3f3f3f3f;
+template<> constexpr ll inf<ll> = 0x3f3f3f3f3f3f3f3f;
+inline void ignoreline(){string ignore; getline(cin, ignore);}
+template<typename T, typename S> constexpr bool chmin(T &a, const S b){if(a>b) return a=b, true; return false;}
+template<typename T, typename S> constexpr bool chmax(T &a, const S b){if(a<b) return a=b, true; return false;}
+template<typename T> void print(vector<T> v){for (auto e : v) cout << e << ' '; cout << '\n';}
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
