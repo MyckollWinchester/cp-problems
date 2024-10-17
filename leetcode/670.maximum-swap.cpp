c3d32d712c8236645533e85e7ec847a08de0f93d
MyckollWinchester <myckoll.winchester@usm.cl>
2024-10-17 01:58:32 -0300

    add new solution

---
+++ leetcode/670.maximum-swap.cpp
@@ -0,0 +1,30 @@
+/*
+* @lc app=leetcode id=670 lang=cpp
+* [670] Maximum Swap
+* https://leetcode.com/problems/maximum-swap
+* Wed Oct 16 2024, 22:00:09 (UTC-3)
+* Myckoll Winchester
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int maximumSwap(int num) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        string s1 = to_string(num);
+        string s2 = s1;
+        sort(s2.begin(), s2.end(), greater<int>());
+        if (s1 == s2)
+            return num;
+        int i = 0;
+        while (s1[i] == s2[i])
+            ++i;
+        auto it = find(s1.rbegin(), s1.rend() - i - 2, s2[i]);
+        swap(s1[i], *it);
+        return stoi(s1);
+    }
+};
+// @lc code=end
