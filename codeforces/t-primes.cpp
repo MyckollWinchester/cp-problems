b022ccba7652f5695407babd3ef39ecd8ff2ab73
MyckollWinchester <myckoll.winchester@usm.cl>
2025-09-10 01:22:56 -0300

    added header to Codeforces problem 230/B

--- codeforces/t-primes.cpp
+++ codeforces/t-primes.cpp
@@ -1,24 +1,25 @@
+/*
+* T-primes
+* https://codeforces.com/problemset/problem/230/B
+* Wed Sep 10 2025, 01:09:02 (UTC-3)
+* Myckoll Winchester
+*/
 #include <bits/stdc++.h>
-
 using namespace std;
 
 typedef unsigned long long ull;
 
 bool primes[999863] = { 0, 0, 1, 1, 0, 1, 0 };
 
-bool isPrime(ull n) {
+bool is_prime(ull n) {
     if (primes[n])
         return true;
-    
     ull range = sqrt(n);
-
     if (n % 2 == 0 || n % 3 == 0) 
         return false; 
-
     for (int i = 5; i <= range; i += 6) 
         if (n % i == 0 || n % (i + 2) == 0) 
             return false; 
-    
     primes[n] = 1;
     return true;
 }
@@ -34,7 +35,7 @@ void solve() {
             cout << "NO\n";
             continue;
         }
-        if (isPrime(sq)) {
+        if (is_prime(sq)) {
             cout << "YES\n";
             continue;
         }
